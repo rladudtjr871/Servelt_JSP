351f52e4f59d923057f26430a34570c2e15adeed
rladudtjr871 <rladudtjr871@naver.com>
2022-07-10 23:04:29 +0900

    "0710"

---
+++ ToyProject/src/main/java/com/test/toy/board/BoardDTO.java
@@ -0,0 +1,41 @@
+package com.test.toy.board;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
+public class BoardDTO {
+	
+	private String seq;
+	private String subject;
+	private String content;
+	private String id;
+	private String regdate;
+	private String readcount;
+	
+	private String name;
+	private String commentcount;
+	
+	private int thread;
+	private int depth;
+	
+	private double isnew;
+	
+	private String filename;
+	private String orgfilename;
+	
+	private ArrayList<String> taglist;
+	
+	private String good;
+	private String bad;
+	private String goodbad; //위에 다 개별
+	
+//	private int cnt; //집합
+	
+	
+	
+	
+	
+	
+}
